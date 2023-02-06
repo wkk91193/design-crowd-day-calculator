9334f23aead3ff27f4af744f984f51c7149bb845
Kumaranath Fernando <wkk91193>
2023-02-06 21:27:16 +1100

    Initial project setup

---
+++ src/day-counter.ts
@@ -0,0 +1,14 @@
+export default class DayCounter {
+
+     weekdaysBetweenTwoDates(firstDate: Date, secondDate: Date): number {
+    // todo
+    
+    return -1
+    }
+
+     businessDaysBetweenTwoDates( firstDate: Date, secondDate: Date, publicHolidays: Date[]): number {       
+        // todo
+        return -1;
+    } 
+
+}
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+const start = async() :Promise<void> =>{
+  console.log('Hello world!');
+}
+
+start();
