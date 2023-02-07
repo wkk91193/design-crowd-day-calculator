0a34d30666e16b16e1e40b6caa78c2823bbf282c
Kumaranath Fernando <wkk91193>
2023-02-07 20:39:41 +1100

    Added logic for getWeekdaysBetweenTwoDates

--- src/day-counter.spec.ts
+++ src/day-counter.spec.ts
@@ -6,8 +6,12 @@ describe('DayCounter',()=>{
         expect(dayCounter.getWeekdaysBetweenTwoDates(new Date('2013-10-07'),new Date('2013-10-09'))).toBe(1)
     });
 
-    it('should return number of weekdays between given when given weekday and weekend day',()=>{
+    it('should return number of weekdays with a timezone offset (one with daylight savings and one with non-daylight savings)',()=>{
         expect(dayCounter.getWeekdaysBetweenTwoDates(new Date('2013-10-05'),new Date('2013-10-14'))).toBe(5);
+        expect(dayCounter.getWeekdaysBetweenTwoDates(new Date('2013-10-07'),new Date('2014-01-01'))).toBe(61);
     });
 
+    it('should return 0 when start is date is greater than endDate', ()=>{
+        expect(dayCounter.getWeekdaysBetweenTwoDates(new Date('2013-10-07'),new Date('2013-10-05'))).toBe(0);
+    })
 });
--- src/day-counter.ts
+++ src/day-counter.ts
@@ -1,43 +1,64 @@
-export default class DayCounter {
-  getWeekdaysBetweenTwoDates(firstDate: Date, secondDate: Date): number {
-    // todo
-    //calculateWeekdays
-    let startDate = this.getDateinUTC(firstDate);
-    let endDate = this.getDateinUTC(secondDate);
 
-    startDate = this.addDaystoGivenDate(startDate, 1);
+type DateRange = {
+  startDate: Date;
+  endDate: Date;
+};
+
+export default class DayCounter {
+
+ //Returns the number of weekdays between first date and second date (first and second exclusive)
+  getWeekdaysBetweenTwoDates(firstDate: Date, secondDate: Date): number {
+    let startDate = firstDate;
+    let endDate = secondDate;
+    if(endDate< startDate){
+        return 0;
+    }
+    const normaliseDateRange = this.getNormalisedDatesForDaylightSavings(
+      startDate,
+      endDate
+    );
+    startDate = normaliseDateRange.startDate;
+    endDate = normaliseDateRange.endDate;
+    return this.calculateWeekDays(startDate, endDate);
+  }
+
+  private calculateWeekDays(startDate :Date, endDate :Date): number{
+    let tempDate = this.addDaystoGivenDate(startDate, 1);
     let counter = 0;
-    while (startDate < endDate) {
-      if (this.isWeekDay(startDate)) {
+    while (tempDate < endDate) {
+      if (this.isWeekDay(tempDate)) {
         counter++;
       }
-      startDate = this.addDaystoGivenDate(firstDate, 1);
+      tempDate = this.addDaystoGivenDate(tempDate, 1);
     }
     return counter;
+
   }
 
   private isWeekDay(day: Date): boolean {
-    if (day.getDay() >= 1 && day.getDay() <= 5) {
+    if (day.getUTCDay() >= 1 && day.getUTCDay() <= 5) {
       return true;
     }
     return false;
   }
 
   private addDaystoGivenDate(date: Date, numberOfDays: number): Date {
-    return new Date(date.setDate(date.getDate() + numberOfDays));
+    return new Date(date.setUTCDate(date.getUTCDate() + numberOfDays));
   }
 
-  private getDateinUTC(date: Date): Date {
-    return new Date(
-      Date.UTC(
-        date.getUTCFullYear(),
-        date.getUTCMonth(),
-        date.getUTCDate(),
-        date.getUTCHours(),
-        date.getUTCMinutes(),
-        date.getUTCSeconds()
-      )
-    );
+  private getNormalisedDatesForDaylightSavings(
+    startDate: Date,
+    endDate: Date
+  ): DateRange {
+    let timezoneDiff =
+      endDate.getTimezoneOffset() - startDate.getTimezoneOffset();
+    if (timezoneDiff != 0) {
+      // Handle daylight saving time difference between two dates.
+      startDate.setMinutes(startDate.getMinutes() + timezoneDiff);
+      endDate.setMinutes(endDate.getMinutes() + timezoneDiff);
+    }
+
+    return { startDate: startDate, endDate: endDate };
   }
   getBusinessDaysBetweenTwoDates(
     firstDate: Date,
